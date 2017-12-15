2f5a19ddd8c68134356fef7d8bd8b17dc1ab6d57
Thomas Horan <t.v.horan@gmail.com>
2017-12-15 07:14:00 +0000

    Day 15

--- 2017/F#/AdventOfCode/AdventOfCode/Day14.fs
+++ 2017/F#/AdventOfCode/AdventOfCode/Day14.fs
@@ -6,18 +6,30 @@ open System
 module Day14App =                
     open Day10
     
-    let SolvePart1 (input:string)  = 
-            let x = 
-                seq{0..127}
-                |> Seq.map(fun x -> input + "-" + x.ToString())
-                |> Seq.map(fun x-> Day10App.SolvePart2 [0..255] x)
-            
-            let y = 
-                x
-                |> Seq.map(fun x -> x |> Seq.map(fun y -> System.Convert.ToString(Convert.ToInt32(y.ToString(), 16), 2).PadLeft(4, '0')) |> String.Concat )
+    let SolvePart1 (input:string)  =             
+        seq{0..127}
+        |> Seq.map(fun x -> input + "-" + x.ToString())
+        |> Seq.map(fun x-> Day10App.SolvePart2 [0..255] x)
+        |> Seq.map(fun x -> x |> Seq.map(fun y -> System.Convert.ToString(Convert.ToInt32(y.ToString(), 16), 2).PadLeft(4, '0')) |> String.Concat )
+        |> Seq.map(fun x -> x |> Seq.sumBy(fun c -> if c = '1' then 1 else 0 ))|> Seq.sum
 
-            let rowTotals = y|> Seq.map(fun x -> x |> Seq.sumBy(fun c -> if c = '1' then 1 else 0 ))
-            rowTotals|> Seq.sum
+    let SolvePart2 (input : string) =
+        let data = 
+            seq{0..127}
+            |> Seq.map(fun x -> input + "-" + x.ToString())
+            |> Seq.map(fun x-> Day10App.SolvePart2 [0..255] x)
+            |> Seq.map(fun x -> x |> Seq.map(fun y -> System.Convert.ToString(Convert.ToInt32(y.ToString(), 16), 2).PadLeft(4, '0')) |> String.Concat )
+            |> Seq.toArray
+            |> Array.map (fun x -> x |> Seq.toArray |> Array.map(fun y -> int (y.ToString())))
+
+        let rec remove i j =
+            if i >= 0 && i < 128 && j >= 0 && j < 128 && data.[i].[j] = 1 then
+                data.[i].[j] <- 0
+                1 + remove (i + 1) j * remove (i - 1) j * remove i (j + 1) * remove i (j - 1)
+            else
+                0
+
+        [ for i in 0 .. 127 do for j in 0 .. 127 -> remove i j ] |> List.sum
 
     
 
@@ -29,7 +41,15 @@ module Day14Tests =
         Assert.That(Day14App.SolvePart1 "flqrgnkx", Is.EqualTo(8108))
 
     [<Test>]
+    let ``Solve part 2 test `` () =
+        Assert.That(Day14App.SolvePart2 "flqrgnkx", Is.EqualTo(1242))
+
+    [<Test>]
     let ``Solve part 1 `` () =
         Assert.That(Day14App.SolvePart1 "hxtvlmkl", Is.EqualTo(8214))
+
+    [<Test>]
+    let ``Solve part 2`` () =
+        Assert.That(Day14App.SolvePart2 "hxtvlmkl", Is.EqualTo(1093))
 
     
---
+++ 2017/F#/AdventOfCode/AdventOfCode/Day15.fs
@@ -0,0 +1,45 @@
+﻿namespace Day15
+
+open NUnit.Framework
+
+module Day15App =
+
+    let rec generator factor x = 
+        seq{
+            yield (x * factor) % 2147483647L
+            yield!  generator factor (x * factor)
+        }
+    
+    let compare (x, y) = 
+        (x &&& 0xFFFFL) = (y &&& 0xFFFFL)
+
+    let SolvePart1 a b runs =    
+        let gena = generator 16807L a
+        let genb = generator 48271L b
+
+        gena 
+        |> Seq.zip genb
+        |> Seq.take runs
+        |> Seq.filter compare
+        |> Seq.length
+
+    
+
+    
+
+    
+module Day15Tests =
+
+    [<Test>]
+    let ``Solve part quick 1 test `` () =
+        Assert.That(Day15App.SolvePart1 65L 8921L 5, Is.EqualTo(1))
+
+    [<Test>]
+    let ``Solve part 1 test `` () =
+        Assert.That(Day15App.SolvePart1 65L 8921L 40000000, Is.EqualTo(588))
+
+    [<Test>]
+    let ``Solve part 1 `` () =
+        Assert.That(Day15App.SolvePart1 591L 393L 40000000, Is.EqualTo(588))
+
+    
